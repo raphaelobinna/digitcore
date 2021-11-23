f90d69c1ca14d59e8df641e8e1847fcc9dd17aca
Raphael Ogbonnaya <raphaelogbonnaya@gmail.com>
2021-11-23 23:07:58 +0200

    task 2

--- digitcore.js
+++ digitcore.js
@@ -5,3 +5,29 @@ function calc(a, b, operator) {
 
 const result = calc('2', '2', '/') //any operator can be added
 console.log(result)
+
+//task 2
+let Tom = [1, 4, 7, 2, 4];
+let Jack = [3, 4, 2, 4, 4];
+let Output = [1, 2];
+
+
+let tomResult = 0;
+let jackResult = 0;
+let finalResult = [];
+
+Tom.forEach((element, key) => {
+    if (element > Jack[key]){
+        tomResult++;
+    } else if (element < Jack[key]){
+        jackResult++;
+    }
+});
+
+finalResult = [tomResult, jackResult];
+
+if (Output[0] === finalResult[0] && Output[1] === finalResult[1]){
+    console.log('true', Output, finalResult);
+} else {
+    console.log('false', Output, finalResult);
+}
